d4ca31baeef3413b0107896eeda4774e5e6b92e4
Duvan Yesid <yesidrodriguez305@gmail.com>
2024-03-29 20:42:18 -0500

    Add GET methods to the Pizza entity

--- src/main/java/com/spring/pizza/services/PizzaService.java
+++ src/main/java/com/spring/pizza/services/PizzaService.java
@@ -1,23 +1,37 @@
 package com.spring.pizza.services;
 
 import com.spring.pizza.persistence.entity.PizzaEntity;
+import com.spring.pizza.persistence.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PizzaService {
-  private final JdbcTemplate jdbcTemplate;
+//  private final JdbcTemplate jdbcTemplate;
+
+//  @Autowired
+//  public PizzaService(JdbcTemplate jdbcTemplate) {
+//    this.jdbcTemplate = jdbcTemplate;
+//  }
+//
+//  public List<PizzaEntity> getAll() {
+//    return this.jdbcTemplate.query("SELECT * FROM pizza WHERE available = 0", new BeanPropertyRowMapper<>(PizzaEntity.class)); // puede ser bastante útil para una consulta rápida o de prueba evitandonos construir un repositorio o entity manager
+//  }
+
+  private final PizzaRepository pizzaRepository;
 
   @Autowired
-  public PizzaService(JdbcTemplate jdbcTemplate) {
-    this.jdbcTemplate = jdbcTemplate;
+  public PizzaService(PizzaRepository pizzaRepository) {
+    this.pizzaRepository = pizzaRepository;
   }
 
   public List<PizzaEntity> getAll() {
-    return this.jdbcTemplate.query("SELECT * FROM pizza WHERE available = 0", new BeanPropertyRowMapper<>(PizzaEntity.class)); // puede ser bastante útil para una consulta rápida o de prueba evitandonos construir un repositorio o entity manager
+    return this.pizzaRepository.findAll();
+  }
+
+  public PizzaEntity get(int idPizza) {
+    return this.pizzaRepository.findById(idPizza).orElse(null);
   }
 }
--- src/main/java/com/spring/pizza/web/controller/PizzaController.java
+++ src/main/java/com/spring/pizza/web/controller/PizzaController.java
@@ -1,10 +1,12 @@
 package com.spring.pizza.web.controller;
 
+import ch.qos.logback.classic.Logger;
 import com.spring.pizza.persistence.entity.PizzaEntity;
 import com.spring.pizza.services.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +16,7 @@ import java.util.List;
 @RequestMapping("/api/pizzas")
 public class PizzaController {
   private final PizzaService pizzaService;
+  private Logger log;
 
   @Autowired
   public PizzaController(PizzaService pizzaService) {
@@ -23,5 +26,11 @@ public class PizzaController {
   @GetMapping
   public ResponseEntity<List<PizzaEntity>> getAll() {
     return ResponseEntity.ok(this.pizzaService.getAll());
+  }
+
+
+  @GetMapping("/{idPizza}")
+  public ResponseEntity<PizzaEntity> get(@PathVariable int idPizza) {
+    return ResponseEntity.ok(this.pizzaService.get(idPizza));
   }
 }
